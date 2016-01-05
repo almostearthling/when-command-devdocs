5ae537c4ac2fe3be3bafc18917d46958dfb4dafd
almostearthling <franz.g@infinito.it>
2016-01-05 19:17:38 +0100

    New logo

--- conf.py
+++ conf.py
@@ -125,7 +125,7 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/when-command.png'
+html_logo = '_static/when-command-dev.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
